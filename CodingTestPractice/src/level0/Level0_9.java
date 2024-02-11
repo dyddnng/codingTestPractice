847854a77dde09fa8ae736c81ba014355997f283
dyddnng <dyddnng@naver.com>
2024-02-11 22:30:31 +0900

    HelloWorld!

---
+++ CodingTestPractice/src/level0/Level0_9.java
@@ -0,0 +1,13 @@
+package level0;
+
+import java.util.Scanner;
+
+public class Level0_9 {
+	/* 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요. */
+	public static void main(String[] args) {
+		 Scanner sc = new Scanner(System.in);
+	        String a = sc.next();
+	        System.out.println(a);
+	}
+
+}
