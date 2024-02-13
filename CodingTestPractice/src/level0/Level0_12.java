5fe19ea6981b10271e8db12dc5dd9420fa532d60
dyddnng <dyddnng@naver.com>
2024-02-14 00:07:15 +0900

    Level0_12

---
+++ CodingTestPractice/src/level0/Level0_12.java
@@ -0,0 +1,20 @@
+package level0;
+
+import java.util.Scanner;
+
+public class Level0_12 {
+	/*
+	 * 영어 알파벳으로 이루어진 문자열 str이 주어집니다. 각 알파벳을 대문자는 소문자로 소문자는 대문자로 변환해서 출력하는 코드를 작성해
+	 * 보세요.
+	 */
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        
+        String result = "";
+        
+        //우선 각각 변환에 있어서는 소문자는 toUpperCase
+        // 대문자는 toLowerCase 함수를 써야한다...!
+	}
+
+}
