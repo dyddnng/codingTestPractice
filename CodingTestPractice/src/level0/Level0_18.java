293828e0ca5ef789b0b3a5139f7a9a9bd4026d63
dyddnng <dyddnng@naver.com>
2024-03-02 15:39:50 +0900

    Level0_18

---
+++ CodingTestPractice/src/level0/Level0_18.java
@@ -0,0 +1,19 @@
+package level0;
+
+public class Level0_18 {
+	/*
+	 * 문자열 my_string, overwrite_string과 정수 s가 주어집니다. 문자열 my_string의 인덱스 s부터
+	 * overwrite_string의 길이만큼을 문자열 overwrite_string으로 바꾼 문자열을 return 하는 solution 함수를
+	 * 작성해 주세요.
+	 */
+	   public String solution(String my_string, String overwrite_string, int s) {
+	        String answer = "";
+	        int a = overwrite_string.length();
+	        int b = my_string.length();
+	        
+	        answer = my_string.substring(0,s) + overwrite_string + my_string.substring(s+a,b);
+	        
+	        return answer;
+	    }
+
+}
